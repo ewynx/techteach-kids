3b51373923cc70e6adfe24050d5f1aa8048f2cb8
Fuentes <E.Fuentes.Bongenaar@protonmail.com>
2020-06-26 17:53:48 -0600

    A start with the SammiBot. The main page is gibberish for now :)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="line"></div>
+    <div class="wrapper">
+      <header role="banner">
+        <nav role="navigation">
+          <h1><a href="#">Asperion</a></h1>
+          <ul class="nav-ul">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Technologies</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contacts</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main role="main">
+        <section class="sec-intro" role="section">
+          <h1>Be Innovative!</h1>
+        </section>
+        <section class="sec-events" role="section">
+          <hr />
+          <h1>Upcomming events</h1>
+          <div>
+            <h1>Tire Technology Expo</h1>
+            <p>Tire Technology Expo has grown in strength and stature every year since it was started in Europe 15 years ago. Visitors and exhibitors to the conference and exhibition staged in February 2014 universally praised it as the 'world's leading tire design and tire manufacturing event', noting in particular the outstanding quality of the conference papers and speakers, and the comprehensive extent of machinery manufacturers and suppliers who exhibited at the event.</p>
+            <a class="link" href="#">more...</a>
+          </div>
+          <div>
+            <h1>Meteorology World Expo</h1>
+            <p>Meteorological Technology World Expo is a truly international exhibition of the very latest climate, weather and hydro - meteorological forecasting, measurement and analysis technologies and service providers for a global community of key decision makers within the aviation industry, shipping companies, marine / port installations, airports, military operations, off-shore exploration companies, wind farm operators, met offices, agriculture operations and research institutes.</p>
+            <a class="link" href="#">more...</a>
+          </div>
+          <div>
+            <h1>Wearable Tech Expo</h1>
+            <p>See live demos, listen to case studies, speak with Wearable Tech Experts The first Wearable Tech Expo in Tokyo 2014. Key players from Japan, America and Europe announced their new products and attracted attention from all over the world. The next Wearable Tech Expo in Tokyo will be doubling in size and include Robotics and IoT. The main players in the wearable industry, human factor engineers, brain scientists, media providers and creators will discuss the future of wearable technology! </p>
+            <a class="link" href="#">more...</a>
+          </div>
+          <div>
+            <h1>Space Tech Expo</h1>
+            <p>Space Tech Expo is the West Coast's premier B2B space event for spacecraft, satellite, launch vehicle and space-related technologies. Taking place in Long Beach, the Space Tech Expo exhibition and conference brings together global decision-makers involved in the design, build and testing of spacecraft, satellite, launch vehicle and space-related technologies. Leading the West Coast space and satellite industry, Space Tech Expo is where end-users connect with solution providers.</p>
+            <a class="link" href="#">more...</a>
+          </div>
+        </section>
+        <section class="sec-projects" role="section">
+          <hr />
+          <h1>Previous Projects</h1>
+          <div>
+            <h1>Neural network for Google</h1>
+            <p>People from Google approached us this January with offer to create new neural network for whole Google's ecosystem. This idea was very interesting and looked almost impossible at first glance. However, our engineers proved their expertise and built amazing autonomous platform. <a class="link" href="#">more...</a></p>
+          </div>
+          <div>
+            <h1>Faster operating system for Apple</h1>
+            <p>For a years, since founding days of a company, Apple always worked with its own operating system. In October, this was about to change. Our company got chance to rewrite the history by creating brand new OS for Apple. It was very difficult and all our employees worked hard every day. <a class="link" href="#">more...</a></p>
+          </div>
+          <div>
+            <h1>Manufacturing technology for Intel</h1>
+            <p>Intel has been manufacturing company for many years. They always used the latest technologies to achieve the best results with lowest costs. However, this was not enough. Intel decided to offer us something that could disrupt whole technology sector. <a class="link" href="#">more...</a></p>
+          </div>
+        </section>
+        <section class="sec-standards" role="section">
+          <hr />
+          <h1>Our Standards</h1>
+          <div>
+            <h1>Excellence</h1>
+            <p>Our goal is to give our customers the best solutions in highest aquality. Our employees strive for excellence and work hard to achieve it. Everything must work and be reliable. Great design must be both, aesthetic and functional. Product or service that pass these conditions will be everlasting.</p>
+          </div>
+          <div>
+            <h1>Uniqueness</h1>
+            <p>Our mantra is to create new paths. We are not interested in copying others because we know, that if you want to succeed, you have to be different, unique. Our customers know that we create unique products and experience for them. This is the best way to differentiate us and our brand from the noise on market. Unique, different and proud.</p>
+          </div>
+        </section>
+      </main>
+    </div>
+    <footer>
+      <nav role="navigation">
+        <ul class="nav-ul">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Technologies</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contacts</a></li>
+        </ul>
+      </nav>
+      <p class="copy">&copy; Alex Devero Design 2014</p>
+    </footer>
+    <div class="line"></div>
+
+<!--    This is what it's really about!-->
+    <ChatBox class="fixed-bottom mb-5"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import ChatBox from './components/ChatBox.vue'
+  export default {
+    name: 'app',
+    components: {
+      ChatBox
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+  }
 </style>
---
+++ src/components/ChatBox.vue
@@ -0,0 +1,150 @@
+<template>
+    <b-container fluid>
+        <b-row align-h="end">
+            <b-col cols="9" sm="7" md="5" lg="4" xl="3" id="chatbox">
+                <b-row class="justify-content-start mb-2" align-v="center" id="sammi-title">
+                    <b-col cols="3 pr-0">
+                        <b-img fluid src="https://content.fortune.com/wp-content/uploads/2019/01/boo.jpg"
+                               rounded="circle" alt="SammiBot" class="mb-1 mt-1"></b-img>
+                    </b-col>
+                    <b-col>
+                        Chatea con Sammi
+                    </b-col>
+                </b-row>
+                <b-container fluid id="chatarea" ref="chatArea" class="p-0">
+                    <b-row v-for="(message, idx) in messages"
+                           :key="idx"
+                           v-bind:class="[ message.isSammi ? 'justify-content-start' : 'justify-content-end']"
+                           class="m-0 mt-1"
+                    >
+                        <b-col class="p-2 message"
+                               cols="auto"
+                               v-bind:class="[ message.isSammi ? 'sammi' : 'client']"
+                               align-self="end">
+                            <span>{{ message.text }}</span>
+                        </b-col>
+                    </b-row>
+                </b-container>
+                <b-input-group class="mt-3 mb-3">
+                    <b-form-input type="text"
+                                  v-model="message"
+                                  @keyup.enter="sendMessage"></b-form-input>
+                    <b-input-group-append>
+                        <b-button @click="sendMessage">Enviar</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: 'ChatBox',
+        data: () => ({
+            message: '',
+            messages: []
+        }),
+        created() {
+            this.messages.push({
+                // TODO change. Esto es para empezar la conversacion.
+                text: 'Hola, soy Sammi',
+                isSammi: true
+            });
+        },
+        methods: {
+            sendMessage() {
+                const message = this.message;
+                this.messages.push({
+                    text: message,
+                    isSammi: false
+                });
+                this.message = '';
+                // TODO put into some config
+                let baseURL = "https://api.dialogflow.com/v1/query?v=20150910";
+                let token = "8103db856e484df6951b448f2ca9ce47";
+
+                const headers = {
+                    'Authorization': 'Bearer ' + token,
+                };
+                let data = {
+                    query: message,
+                    lang: 'en',
+                    // TODO change to real sessionid
+                    sessionId: '12345'
+                };
+                axios.post(baseURL, data, {headers})
+                    .then(res => {
+                        // Get the messages for Sammi and filter out the empty ones
+                        let responseMessages = res.data.result.fulfillment.messages.filter(item => item.speech);
+                        responseMessages.forEach(async message => {
+                            this.messages.push({
+                                text: message.speech,
+                                isSammi: true
+                            });
+                        });
+                        this.$nextTick(() => {
+                            let messageDisplay = this.$refs.chatArea;
+                            messageDisplay.scrollTop = messageDisplay.scrollHeight;
+                        });
+                    });
+            }
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    #chatbox {
+        box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
+        /*created en https://cssgradient.io/*/
+        background: rgb(40,44,130);
+        background: linear-gradient(351deg, rgba(40,44,130,1) 0%, rgba(79,83,158,1) 35%, rgba(165,180,229,1) 100%);
+    }
+
+    #chatarea {
+        max-height: 50vh;
+        overflow: auto;
+
+    }
+
+    #sammi-title {
+        background: #A5B4E5;
+        text-align: start;
+        color: white;
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    .container {
+        border-color: black;
+        border-width: 1px;
+    }
+
+    .message {
+        border-radius: 8px;
+    }
+
+    .sammi {
+        text-align: start;
+        background: white;
+        color: rgb(40, 44, 130);
+    }
+
+    .client {
+        text-align: end;
+        background: #F8EB7C;
+    }
+
+    .btn {
+        background-color: #A5B4E5;
+        border-width: 0;
+        color: black;
+    }
+
+    .form-control:focus {
+        border-color: #ced4da;
+        box-shadow: none;
+    }
+</style>
