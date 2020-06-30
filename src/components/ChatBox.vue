66e0d11036be6adb2b44c46ab4a7f7b7df4af78d
Fuentes <E.Fuentes.Bongenaar@protonmail.com>
2020-06-30 14:53:09 -0600

    Upon receiving a class suggestion from SammiBot, show this as a thumbnail. A click should take the user to that class. Needs details, and especially some sort of mapping of the class codes to thumbnails and links.

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -17,13 +17,23 @@
                            v-bind:class="[ message.isSammi ? 'justify-content-start' : 'justify-content-end']"
                            class="m-0 mt-1"
                     >
-                        <b-col class="p-2 message"
+                        <b-col v-if="!message.isTTClass"
+                               class="p-2 message"
                                cols="auto"
                                v-bind:class="[ message.isSammi ? 'sammi' : 'client']"
                                align-self="end">
                             <span>{{ message.text }}</span>
                         </b-col>
+                        <b-col v-if="message.isTTClass"
+                               class="p-2"
+                               cols="auto"
+                               align-self="end">
+                            <b-link class="tt-class"  :href="message.referral">
+                                <b-img thumbnail :src="message.img"></b-img>
+                            </b-link>
+                        </b-col>
                     </b-row>
+
                 </b-container>
                 <b-input-group class="mt-3 mb-3">
                     <b-form-input type="text"
@@ -55,10 +65,14 @@
             });
         },
         methods: {
+            thumbUrl(filename) {
+                return require(`../assets/images/thumbnails/${filename}`);
+            },
             sendMessage() {
                 const message = this.message;
                 this.messages.push({
                     text: message,
+                    isTTClass: false,
                     isSammi: false
                 });
                 this.message = '';
@@ -79,12 +93,32 @@
                     .then(res => {
                         // Get the messages for Sammi and filter out the empty ones
                         let responseMessages = res.data.result.fulfillment.messages.filter(item => item.speech);
-                        responseMessages.forEach(async message => {
-                            this.messages.push({
-                                text: message.speech,
-                                isSammi: true
+                        let responseMessagesCustom = res.data.result.fulfillment.messages.filter(item => item.payload);
+
+                        if ( responseMessagesCustom.length > 0 ) {
+                            responseMessagesCustom.forEach(async message => {
+                                this.messages.push({
+                                    text: message.payload.title,
+                                    isTTClass: false,
+                                    isSammi: true
+                                });
+                                this.messages.push({
+                                    img: this.thumbUrl(message.payload.ttclass + '.jpg'), //TODO we should create a system to get img and referral for a specific "code class"
+                                    referral: 'https://www.nos.nl', //TODO This would be a referral to our own website
+                                    isTTClass: true,
+                                    isSammi: true
+                                });
                             });
-                        });
+                        } else {
+                            responseMessages.forEach(async message => {
+                                this.messages.push({
+                                    text: message.speech,
+                                    isTTClass: false,
+                                    isSammi: true
+                                });
+                            });
+                        }
+
                         this.$nextTick(() => {
                             let messageDisplay = this.$refs.chatArea;
                             messageDisplay.scrollTop = messageDisplay.scrollHeight;
@@ -99,8 +133,8 @@
     #chatbox {
         box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
         /*created en https://cssgradient.io/*/
-        background: rgb(40,44,130);
-        background: linear-gradient(351deg, rgba(40,44,130,1) 0%, rgba(79,83,158,1) 35%, rgba(165,180,229,1) 100%);
+        background: rgb(40, 44, 130);
+        background: linear-gradient(351deg, rgba(40, 44, 130, 1) 0%, rgba(79, 83, 158, 1) 35%, rgba(165, 180, 229, 1) 100%);
     }
 
     #chatarea {
@@ -135,6 +169,11 @@
     .client {
         text-align: end;
         background: #F8EB7C;
+    }
+
+    .tt-class {
+        background-color: transparent!important;
+        padding: 0;
     }
 
     .btn {
